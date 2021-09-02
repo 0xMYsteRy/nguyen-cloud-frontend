2897ec70656723acf7b87490cb1e45c17cad6f21
0xMYsteRy <s3777242@rmit.edu.vn>
2021-09-02 22:00:19 +0700

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # How to run
 
 ## Install all the libraries
-npm install -save axios react-chartjs-2 chart.js react-countup classnames @material-ui/core
+npm install -save axios react-chartjs-2 chart.js react-countup classnames @material-ui/core @material-ui/data-grid @@material-ui/icons
 
 ## Run the local server
 npm start
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 export {default as Cards} from './Cards/Cards';
 export {default as Chart} from './Chart/Chart';
 export {default as CountryPicker} from './CountryPicker/CountryPicker';
-
+export {default as Sidenav} from './Sidenav/Sidenav';
+export {default as Country} from './Country/Country';
+export {default as Register} from './Register/Register';
+export {default as Vaccine} from './Vaccine/Vaccine';
+export {default as Review} from './Review/Review';
